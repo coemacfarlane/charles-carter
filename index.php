bc0f50c239711948890f62425a8302c42314c240
Coe M <madisonmacf@gmail.com>
2019-12-12 19:59:22 -0500

    wordpress config

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta property="og:title" content="The Charles Carter" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="/imgs/logo_block_header.png" />
+    <title>Charles Carter Home</title>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="css/style.css"
+      media="screen"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Cormorant+Garamond&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://use.typekit.net/anr7skv.css" />
+
+    <script src="/js/script.js"></script>
+  </head>
+  <body>
+    <header>
+      <div class="nav"></div>
+
+      <a id="title" href="issue.html">
+        <h1>Fall</h1>
+        <h1>2019</h1>
+      </a>
+      <p id="label">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis
+      </p>
+      <a id="cover" href="issue.html"
+        ><img src="imgs/temp_cover.png" alt="issue cover"
+      /></a>
+    </header>
+
+    <p>
+      The Charles Carter is a literary magazine run by students at UNC Chapel
+      Hill. We publish poetry and fiction in biannual issues.
+    </p>
+
+    <section id="news">
+      <h2>News & Events</h2>
+      <div id="carousel">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+    </section>
+
+    <section id="anthology">
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+      <div class="card"></div>
+    </section>
+
+    <footer>
+      <p>The Charles Carter Literary Magazine</p>
+      <p>A Chapel Hill Student Organization est. 2015</p>
+    </footer>
+  </body>
+</html>
