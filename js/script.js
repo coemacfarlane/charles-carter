655bdadb857334ac429ebfab23645979b732f571
Coe M <madisonmacf@gmail.com>
2019-12-29 16:41:16 -0500

    home header visual/layout update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// OPEN/CLOSE DROPDOWN MENU FROM HAMBURGER BUTTON
+// OPEN/CLOSE DROPDOWN MENU FROM HAMBURGER BUTTON       OPEN/CLOSE DROPDOWN MENU FROM HAMBURGER BUTTON
 // GET ELEMENTS
 var btn = document.getElementById("dropdown_btn"); // button pressed to open menu
 var span = document.getElementsByClassName("hamburger"); // the 3 span elements representing btn
@@ -11,18 +11,14 @@ btn.onclick = function() {
   if (open == false) {
     // OPEN MENU
     dropdown.style.left = "50vw";
-    span[0].classList.toggle("close");
-    span[1].classList.toggle("close");
-    span[2].classList.toggle("close");
-
     open = true;
   } else {
     //CLOSE MENU
     dropdown.style.left = "100vw";
-    span[0].classList.toggle("close");
-    span[1].classList.toggle("close");
-    span[2].classList.toggle("close");
-
     open = false;
+  }
+  // TOGGLE CLOSE CLASS FOR EACH HAMBURGER SPAN ELT
+  for (var i = 0; i < span.length; i++) {
+    span[i].classList.toggle("close");
   }
 };
